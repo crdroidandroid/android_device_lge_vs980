4e00a5931afbbcfee09ab2b3819f719e4d6e4230
firebird11 <hbgassel@gmail.com>
2015-09-24 08:27:46 +0200

    Defconfig: follow crdroid

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,7 +16,7 @@
 
 include device/lge/g2-common/BoardConfigCommon.mk
 
-TARGET_KERNEL_CONFIG := cyanogenmod_vs980_defconfig
+TARGET_KERNEL_CONFIG := crdroid_vs980_defconfig
 
 TARGET_OTA_ASSERT_DEVICE := vs980,g2,galbi
 
