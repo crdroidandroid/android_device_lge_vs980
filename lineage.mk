9382789b89e9b89367374ca2c1f0c26ee7503325
firebird11 <hbgassel@gmail.com>
2017-01-07 11:57:39 +0100

    vs980: rebrand to Lineage
    Change-Id: I8516e4a4cb969231139ab75f890c02e3048b3ca3

--- lineage.mk
+++ lineage.mk
@@ -6,7 +6,7 @@ $(call inherit-product, device/lge/vs980/vs980.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vs980
-PRODUCT_NAME := cm_vs980
+PRODUCT_NAME := lineage_vs980
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-VS980
 PRODUCT_MANUFACTURER := lge
